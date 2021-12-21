43568f4649da5eb7d036f606d95ed3f8bc64a24a
Masadavide <davide.masadm@gmail.com>
2021-12-21 17:10:57 +0100

    funzionegenera numeri casuali non ripetibili

--- myscript.js
+++ myscript.js
@@ -1,4 +1,14 @@
+/* Consegna
+L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
+
+
 let btn = document.getElementById("btn-play");
+let bomb = [];
+console.log(arrayBomb())
 
 btn.addEventListener("click",function(){
     let grid = document.getElementById('grid');
@@ -17,15 +27,41 @@ btn.addEventListener("click",function(){
             grid.innerHTML += `<div class="box box3 d-flex justify-content-center align-items-center">${i}</div>`
         }
     }
-
-    let box = document.getElementsByClassName('box')
-    for (i = 0; i < box.length; i++)
-        box[i].addEventListener("click",function(){
-            if (this.classList.contains("safe")){
-                this.classList.remove("safe")
-            }else{
+    
+    let box = document.getElementsByClassName('box');
+        for (i = 0; i < box.length; i++) {
+            box[i].addEventListener("click",function(){
                 this.classList.add("safe")
                 console.log(this.innerHTML);
-            }
-    })
+            })
+        }
 })
+
+
+
+/* Consegna
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà 
+prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l’utente clicca su una cella: se il numero è presente nella lista dei 
+numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la 
+partita termina, altrimenti la cella cliccata si colora di azzurro e l’utente può 
+continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge il numero 
+massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero 
+di volte che l’utente ha cliccato su una cella che non era una b. */
+
+function arrayBomb(){
+    for(i = 0; i < 16;){
+        let num = Math.floor((Math.random())* 100) + 1;
+        console.log(num)
+        if(!bomb.includes(num)){
+            i++;
+            bomb.push(num)
+        }
+    }
+    console.log(bomb);
+    return bomb
+}
+
