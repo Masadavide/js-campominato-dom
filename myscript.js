1d9da728ea64974bebcd85090d20fdab29338d42
Masadavide <davide.masadm@gmail.com>
2021-12-23 00:34:23 +0100

    da sistemare

--- myscript.js
+++ myscript.js
@@ -5,40 +5,6 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
 
-
-let btn = document.getElementById("btn-play");
-let bomb = [];
-console.log(arrayBomb())
-
-btn.addEventListener("click",function(){
-    let grid = document.getElementById('grid');
-    let level =  document.getElementById('level').value;
-    grid.innerHTML = "";
-    if(level == 1){
-        for(i = 1; i <= 100; i++){
-            grid.innerHTML += `<div class="box box1 d-flex justify-content-center align-items-center">${i}</div>`
-        }
-    }else if(level == 2){
-        for(i = 1; i <= 81; i++){
-            grid.innerHTML += `<div class="box box2 d-flex justify-content-center align-items-center">${i}</div>`
-        }
-    }else{
-        for(i = 1; i <= 49; i++){
-            grid.innerHTML += `<div class="box box3 d-flex justify-content-center align-items-center">${i}</div>`
-        }
-    }
-    
-    let box = document.getElementsByClassName('box');
-        for (i = 0; i < box.length; i++) {
-            box[i].addEventListener("click",function(){
-                this.classList.add("safe")
-                console.log(this.innerHTML);
-            })
-        }
-})
-
-
-
 /* Consegna
 Il computer deve generare 16 numeri casuali nello stesso range della difficoltà 
 prescelta: le bombe.
@@ -52,16 +18,60 @@ massimo possibile di numeri consentiti.
 Al termine della partita il software deve comunicare il punteggio, cioè il numero 
 di volte che l’utente ha cliccato su una cella che non era una b. */
 
-function arrayBomb(){
-    for(i = 0; i < 16;){
+
+let btn = document.getElementById('btn-play');
+let grid = document.getElementById('grid');
+let bomb = [];
+console.log(bomb);
+
+function generaGriglia (numeroCelle){
+    for(i = 1; i <= numeroCelle; i++){
+        grid.innerHTML += `<div class="box d-flex justify-content-center align-items-center">${i}</div>`
+    }
+}
+
+btn.addEventListener("click", function(){
+    let box = document.getElementsByClassName('box');
+    console.log(box)
+    let level =  document.getElementById('level').value;
+    grid.innerHTML = "";
+
+    if(level == 1){
+        generaGriglia(100);
+        
+        box.setAttribute('box1');
+    }else if(level == 2){
+        generaGriglia(81);
+        box.classList.add('box2');
+    }else{
+        generaGriglia(49);
+        box.classList.add('box3');
+    }
+    
+    generaBomba(16);
+    
+    for (i = 0; i < box.length; i++) {
+        box[i].addEventListener("click",function(){
+            const numeroCella = parseInt(this.innerHTML);
+            this.classList.add("safe")
+            console.log(this.innerHTML);
+            if(bomb.includes(numeroCella)) {
+                console.log("hai trovato una bomba")
+            }
+        })
+    }
+    
+})
+
+
+
+
+
+function generaBomba(num){
+    while(bomb.lenght < num) {
         let num = Math.floor((Math.random())* 100) + 1;
-        console.log(num)
         if(!bomb.includes(num)){
-            i++;
             bomb.push(num)
         }
     }
-    console.log(bomb);
-    return bomb
 }
-
